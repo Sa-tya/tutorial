ce3e2849c98ecf73c763f49826e6c271c4c34474
Satya Prakash <80380695+Sa-tya@users.noreply.github.com>
2021-08-22 02:38:52 +0530

    Bug fixed

--- src/E-commerce/landingPage/history.js
+++ src/E-commerce/landingPage/history.js
@@ -1,22 +1,25 @@
 import { useSelector } from "react-redux";
-
+import booksname from "../header/bookname";
 function History() {
     const images = useSelector(state => state.data)
+    var order;
     if(localStorage.getItem('order'))
-    var order = JSON.parse(localStorage.getItem('order'))
+    order = JSON.parse(localStorage.getItem('order'))
+    else
+    order = {};
 
     return <div className='catcontainer'>
-        {order ? <h1>Your Shoping History</h1> : <h1>You have not Purchached anything</h1>}
+        {Object.keys(order).length ? <h1>Your Shoping History</h1> : <h1>You have not Purchached anything</h1>}
         <div className='cartContainer'>
             {Object.keys(order).map((itm, indx) => <div key={indx} className='cartCard'>
                 <div className='cartimg'>
                     <img src={images[itm].default} alt='order' />
                 </div>
                 <div>
-                    <h2>Title</h2>
-                    <h3>price</h3>
+                    <h2>{booksname[itm][0]}</h2>
+                    <h3>Rs. {itm}</h3>
                     <h3>Qty - {order[itm].qty}</h3>
-                    <h3>cost</h3>
+                    <h3>cost- {order[itm].qty * itm}</h3>
                 </div>
             </div>
             )}
